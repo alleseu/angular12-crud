115342d228e3c271ccdf25b701a072af494dc403
Alejandro Sánchez <arehandoro.sanchesu@gmail.com>
2021-09-02 22:27:53 -0400

    Versión 0.2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-angular'`, () => {
+  it(`should have as title 'angular12-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-angular');
+    expect(app.title).toEqual('angular12-crud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular12-crud app is running!');
   });
 });
